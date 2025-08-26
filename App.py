1e774b5eb9095277ba44d9c953626bde9f020da0
Yashwanth Gowda KS <yashwanthgowdaksyashugowda@gmail.com>
2025-08-26 17:25:14 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="your_password",
+        password="bus@1234",
         database="busbooking"
     )
 
